95ebebadf190fc38d8a72d2ab77f26a3b377cbf4
jiaxp3 <jiaxp3@lenovo.com>
2021-06-21 14:56:28 +0800

    commit

--- env.js
+++ env.js
@@ -1,13 +1,13 @@
 module.exports = {
-  VUE_APP_TEMPLATE_API: '"/templateapi"',
-  VUE_APP_FIELDSERVICE_API: '"/fieldserviceapi"',
-  VUE_APP_AUTHORITY_API: '"/authorityapi"',
-  VUE_APP_PARTMGT_API: '"/partmgtapi"',
-  VUE_APP_PRODUCT_API: '"/productapi"',
-  VUE_APP_MAGICCUBe_API: '"/api"',
-  VUE_APP_TENANTCENTER_API: '"/tenantcenterapi"',
-  VUE_APP_DATAQUERY_API: '"/dataqueryapi"',
-  VUE_APP_QUARTZ_API: '"/cloudquartzjob"',
-  VUE_APP_KETTLE_API: '"/intm"',
-  VUE_APP_MANAGED_SERVICE_API: '"/managedservice"'
+  VUE_APP_TEMPLATE_API: '"/templateapi"', // 表单流程配置 api
+  VUE_APP_FIELDSERVICE_API: '"/fieldserviceapi"', // 现场服务交付 api
+  VUE_APP_AUTHORITY_API: '"/authorityapi"', // 组织人员权限 api
+  VUE_APP_PARTMGT_API: '"/partmgtapi"', // 备件 api
+  VUE_APP_PRODUCT_API: '"/productapi"', // 产品线 api
+  VUE_APP_MAGICCUBe_API: '"/api"', // 魔方 api
+  VUE_APP_TENANTCENTER_API: '"/tenantcenterapi"', // 租户 api
+  VUE_APP_DATAQUERY_API: '"/dataqueryapi"', // 数据查询平台 api
+  VUE_APP_QUARTZ_API: '"/cloudquartzjob"', // 度中心 api
+  VUE_APP_KETTLE_API: '"/intm"', // kettlemanager api
+  VUE_APP_MANAGED_SERVICE_API: '"/managedservice"', // 运维工单 api
 }
